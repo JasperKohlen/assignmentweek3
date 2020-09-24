8854dda72a525a528a3ce8d8dc2352b39d86f017
jkohlen <386329@student.fontys.nl>
2020-09-24 11:23:08 +0200

    first commit

---
+++ Assets/Move.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    public Rigidbody myBody;
+    public float _constantMoveSpeed = 3.0f;
+
+    Vector3 moveDirection = Vector3.zero;
+    // Use this for initialization
+    void Start()
+    {
+        myBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            moveDirection.x = -1.0f;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            moveDirection.x = 1.0f;
+        }
+        else
+        {
+            moveDirection.x = 0.0f;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            myBody.AddForce(Vector3.up * 500.0f);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        myBody.MovePosition(transform.position + moveDirection * _constantMoveSpeed * Time.deltaTime);
+    }
+}
