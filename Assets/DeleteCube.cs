b802ac3509d6b29070cbbc52c67461547e5b2ab9
jkohlen <386329@student.fontys.nl>
2020-09-24 11:23:08 +0200

    first commit

---
+++ Assets/DeleteCube.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeleteCube : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject cube;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Vector3 v = new Vector3(-7, 4, 1);
+        cube.transform.position = v;
+    }
+}
